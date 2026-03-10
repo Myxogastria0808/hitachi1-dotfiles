24140f332dd4a612ccd817ae6108be84898f77ca
Myxogastria0808 <r.rstudio.c@gmail.com>
2026-03-11 00:31:55 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    # nixpkgs
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # home-manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # nixvim
+    nixvim-config.url = "github:Myxogastria0808/nix-flakes-nixvim";
+  };
+
+  outputs = inputs: {
+    ## home-manager ##
+    homeConfigurations = {
+      breaking = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "aarch64-linux";
+          # Enable unfree pkgs
+          config.allowUnfree = true;
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+          username = "hello";
+        };
+        modules = [
+          ./home/home.nix
+        ];
+      };
+    };
+  };
+}
---
+++ home/home.nix
@@ -0,0 +1,21 @@
+{
+  inputs,
+  pkgs,
+  system,
+  username,
+  ...
+}:
+
+{
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+    stateVersion = "25.11"; # Please read the comment before changing.
+  };
+  home.packages = with pkgs; [
+    # nixvim
+    inputs.nixvim-config.packages.nixvim
+  ];
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
